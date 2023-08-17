778459e4630109c7a4d48f0cf6f87404056e9f22
Ace-Kat <92191466+Ace-Kat@users.noreply.github.com>
2023-08-16 20:56:15 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,16 @@ let circles = [];
 let windows = [];
 let superstructureTitle;
 let baseTitle;
+let window1;
+let window2;
+let window3;
+let window4;
+let window5;
+let window6;
+let window7;
+let window8;
+let window9;
+let window10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,7 +30,7 @@ function setup() {
   for (let i = 0; i < 5; i++) {
     let x = map(i, 0, 4, 100, width - 100);
     let y = 3 * windowHeight / 4;
-    circles.push(new Circle(x, y, 90, 'base', `Base ${i + 1}`));
+    circles.push(new Circle(x, y, 95, 'base', `Base ${i + 1}`));
   }
 
 
@@ -55,12 +65,35 @@ function draw() {
 function mouseClicked() {
   for (let circle of circles) {
     if (circle.contains(mouseX, mouseY)) {
-      windows.push(new Window(circle));
+      if (circle === circles[0]) {
+        windows.push(new Window(circle, "Text for Window 1", "image1.jpg"));
+      } else if (circle === circles[1]) {
+        windows.push(new Window(circle, "Text for Window 2", "image2.jpg"));
+      } else if (circle === circles[2]) {
+        windows.push(new Window(circle, "Text for Window 3", "image3.jpg"));
+      } else if (circle === circles[3]) {
+        windows.push(new Window(circle, "Text for Window 3", "image4.jpg"));
+      } else if (circle === circles[4]) {
+        windows.push(new Window(circle, "Text for Window 3", "image5.jpg"));
+      } else if (circle === circles[5]) {
+        windows.push(new Window(circle, "Stable diffusion is also a material expensive ai. In terms of physical infrastructure it requires lots of computational power in terms of CPUs and GPUs which require lots of labor. Software is built on open source platforms based on the labor of many developers. The data is scraped from the internet, so it also depends on the labor of many artists. Finally, it utilizes the internet and the cloud for online use which itself depends on the labor of technicians and developers. (refer to Crawford)", "image6.jpg"));
+      } else if (circle === circles[6]) {
+        windows.push(new Window(circle, "Stable Diffusion was trained on a dataset of images and captions that was scraped from the web by LAION, a German non-profit that receives funding from Stability AI. The dataset includes over 5 billion image-text pairs, which were classified based on language, resolution, watermark likelihood, and aesthetic score. The Stable Diffusion model was trained on three subsets of the LAION-5B dataset: laion2B-en, laion-high-resolution, and laion-aesthetics v2 5+ (website link?). A third-party analysis of the model's training data found that approximately 47% of the images came from 100 different domains, with Pinterest being the largest source (8.5%). Other major sources included WordPress, Blogspot, Flickr, DeviantArt, and Wikimedia Commons. This means that Stable Diffusion is likely to reflect the biases and perspectives of the websites that were used to create the training dataset. For example, the model is more likely to generate images of white people than of people of color, as these groups are more likely to be represented in the images that were scraped from Pinterest and other popular websites. ", "image7.jpg"));
+      }else if (circle === circles[7]) {
+        windows.push(new Window(circle, "Artists currently are already overworked on the basis of competition and digital art. As art becomes more commodified, large companies exploit huge artists to meet the demand that exists (source?). The proof can be currently seen through the Hollywood strike. This is especially fueled by capitalism as there is a huge demand to consume art. As Stable Diffusion and such generative ai become more prominent in the art space. There become a competition between the artists and AI. As AI doesn't need to be paid, it can drive down wages for artists. Not only that, there is a relationship of ownership since currently most of stable diffusion's data set is from human art and it can lead to relationships of exploitation. Furthermore, it can not only compete with artists but other human dominated fields such as medicine.  This will be fueled by capitalism as AI is generally more competitive than human workers in many spaces. ", "image8.jpg"));
+      }else if (circle === circles[8]) {
+        windows.push(new Window(circle, "Stable diffusion creates its art based on prompts from a human operator. This creates a questions of the relationship of labor between the human operator, the AI, and the art piece that is created. How much creativity is from the operator and how much is from the AI. It is more complicated as AI uses data from the internet but at the same time has a distinct style. This can create huge legal problems as well as increase the problem with artist exploitation. ", "image9.jpg"));
+      }else if (circle === circles[9]) {
+        windows.push(new Window(circle, "Stable diffusions is funded by Emad Mostaque, who is the founder of stability.ai. He made his fortunre through being a hedge fund manager (source?). They also raised money from other venture capital firms (source?). As investors, they have huge control over the company and the products. As mentioned before, these firms and people have exploited massive amount of labor to achieve this money and it gets invested into projects that promote their agenda such as TESCrealism instead of ai that can help improve humanity currently.  ", "image10.jpg"));
+      }
+      
+      
       redraw(); // Redraw the canvas after interaction
       break;
     }
   }
 }
+
 
 class Circle {
   constructor(x, y, r, group, label) {
@@ -81,7 +114,7 @@ class Circle {
     fill(this.group === 'superstructure' ? 'blue' : 'green');
     ellipse(this.x, this.y, this.r * 2);
     fill(255);
-    textSize(14);
+    textSize(12);
     textAlign(CENTER, CENTER);
     text(this.label, this.x, this.y);
   }
@@ -92,9 +125,19 @@ class Circle {
 }
 
 class Window {
-  constructor(circle) {
+  constructor(circle, initialText = "Default window text", imageSrc = "") {
     this.circle = circle;
     this.showCloseButton = true;
+    this.textContent = initialText;
+    this.image = loadImage(imageSrc); // Load the image
+  }
+
+  setTextContent(content) {
+    this.textContent = content; // Set the text content for the window
+  }
+
+  updateImage(imageSrc) {
+    this.image = loadImage(imageSrc); // Update the image
   }
 
   display() {
@@ -107,6 +150,25 @@ class Window {
     textSize(20);
     textAlign(CENTER, CENTER);
     text(`${this.circle.label}`, centerX, centerY - 220);
+
+    // Calculate text area position and size
+    let textX = centerX - 550;
+    let textY = centerY - 150;
+    let textWidth = 500; // Adjust the width of the text area
+    let textHeight = 1000;
+
+    fill(0);
+    textSize(16);
+    textAlign(LEFT, TOP);
+    text(this.textContent, textX, textY, textWidth, textHeight);
+
+    if (this.image) {
+      let imageX = centerX + 100; // Adjust the image X position
+      let imageY = centerY - 150; // Adjust the image Y position
+      let imageSize = 400; // Adjust the image size
+
+      image(this.image, imageX, imageY, imageSize, imageSize);
+    }
 
     if (this.showCloseButton) {
       this.displayCloseButton(30, 30); // Position close button at top left corner
